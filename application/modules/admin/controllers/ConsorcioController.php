aae2b1843bb03882399dd3cd03540589fadd9805
prueba <prueba@gmail.com>
2013-05-19 01:21:34 -0500

    sa

--- application/modules/admin/controllers/ConsorcioController.php
+++ application/modules/admin/controllers/ConsorcioController.php
@@ -337,6 +337,76 @@ class Admin_ConsorcioController extends App_Controller_Action_Admin {
         $this->_flashMessenger->addMessage("Contacto eliminado con exito");
         $this->_redirect('/admin/consorcio/contacto/empresa/'.$idEmpresa);
     }
+    
+    public function personalAction()
+    {
+        $idEmpresa = $this->_getParam('empresa');
+        $model= new App_Model_Personal();
+        $lista = $model->listarPersonal($idEmpresa);
+        $this->view->listaExperiencia = $lista;
+        $this->view->empresa = $idEmpresa;
+    }
 
+    public function personalCrearAction()
+    {   
+        $idEmpresa = $this->_getParam('empresa');
+        if (!$idEmpresa)
+            $this->_redirect("/admin/consorcio");
+        $form = new App_Form_CrearPersonal();
+        $this->view->form = $form;
+        $this->view->empresa = $idEmpresa;
+        
+        if($this->getRequest()->isPost()){            
+            
+            $data = $this->getRequest()->getPost();
+            
+            if ($form->isValid($data)) {
+                $model = new App_Model_Personal();
+                $data['fechaRegistro'] = Zend_Date::now()->toString('YYYY-MM-dd HH:mm:ss');
+                $data['estado'] = '1';
+                $data['idEmpresa'] = $idEmpresa;
+                $id = $model->actualizarDatos($data);
+                
+                $this->_flashMessenger->addMessage("Personal Creado Correctamente");
+                $this->_redirect('/admin/consorcio/personal/empresa/'.$idEmpresa);
+                
+            } /*else {
+                $form->populate($data);                
+            }*/
+        }
+    }
+    
+    public function personalEditarAction()
+    {        
+        $model = new App_Model_Personal();
+        $form = new App_Form_CrearPersonal();
+        $id = $this->_getParam('id');
+        $idEmpresa = $this->_getParam('empresa');
+        $experiencia = $model->getPersonalPorId($id);
+        $form->populate($experiencia);        
+        if($this->getRequest()->isPost()){            
+            $data = $this->getRequest()->getParams();            
+            $data['idPersonal'] = $id;
+            //if ($form->isValid($data)) {                
+                $id = $model->actualizarDatos($data);
+                $this->_flashMessenger->addMessage("Personal actualizado con éxito");
+                $this->_redirect('/admin/consorcio/personal/empresa/'.$idEmpresa);
+                
+            /*} else {
+                $form->populate($data);                
+            }*/
+        }
+        $this->view->form = $form;
+    }
+    
+    public function personalEliminarAction()
+    {        
+        $model = new App_Model_Personal();
+        $id = $this->_getParam('id');
+        $idEmpresa = $this->_getParam('empresa');
+        $model->eliminarPersonal($id);
+        $this->_flashMessenger->addMessage("Personal eliminado correctamente");
+        $this->_redirect('/admin/consorcio/personal/empresa/'.$idEmpresa);
+    }
 }
 
